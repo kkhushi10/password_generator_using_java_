1170f4988cf514385540b6bc926442f51932dde5
kkhushi10 <126804998+kkhushi10@users.noreply.github.com>
2023-05-31 22:25:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Random;
+
+public class Main{
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=";
+    private static final int PASSWORD_LENGTH = 10;
+
+    public static void main(String[] args) {
+        String password = generatePassword(PASSWORD_LENGTH);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
